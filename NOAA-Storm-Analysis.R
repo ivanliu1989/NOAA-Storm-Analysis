218495b0c38ec573fc19fc3d7c8974f314c763cf
ivanliu1989 <ivan.liuyanfeng@gmail.com>
2014-08-14 16:25:31 +1000

    data exploration

--- NOAA-Storm-Analysis.R
+++ NOAA-Storm-Analysis.R
@@ -5,8 +5,17 @@
     setwd("C:/Documents and Settings/Macro/Desktop/Ivandata/NOAA-Storm-Analysis")
     data.url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2"
     data.csv <- "repdata-data-StormData.csv"
-    if(!(file.exists(data.zip)))
+    if(!(file.exists(data.csv)))
         download.file(data.url, destfile="repdata-data-StormData.csv")
     noaa.data <- read.csv(data.csv,head=T)
-
+    
 ## Dataset exploration     
+    str(noaa.data)
+    head(noaa.data,n=3)
+    colnames(noaa.data)<-tolower(colnames(noaa.data))
+
+## Subset dataset according to analysis requirement
+    index.col <- c(1:2,7:8,21:28)
+    noaa.sub <- subset(noaa.data, select=index.col)
+    str(noaa.sub)
+    
